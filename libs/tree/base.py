76b9cec5ab4e4fe42089bdffd8b4f1adb8f3e473
felix <felix.martel@polytechnique.edu>
2020-04-17 21:22:48 -0400

    Add better subclassing mechanisms to `libs.tree.Node` with TypeVar

--- libs/tree/base.py
+++ libs/tree/base.py
@@ -9,7 +9,9 @@ Basic tree ideas: a Node is identified to its subtree, that is a Tree can be:
 # TODO: add static creation methods 'from_tree', 'from_revtree', + dump/load methods
 # TODO: add the notion of *tree compression* (subtree containing only selected nodes)
 """
-from typing import List, Union, Dict, Tuple, NewType, Iterable, Callable, Generator, TextIO
+from typing import List, Union, Dict, Tuple, Iterable, Callable, Generator, TextIO,\
+    Generic, Hashable, TypeVar, Optional, Type
+
 from collections import deque
 from contextlib import redirect_stdout
 from io import StringIO
@@ -19,64 +21,74 @@ from libs.tree.pprint import print_tree
 
 INF = float("inf")
 
-NodeId = NewType("NodeId", str)
-NodeOrName = Union[str, NodeId, "Node"]
+T = TypeVar("T", int, str, Hashable)
+TNode = TypeVar("TNode", bound="Node")
+
 Number = Union[int, float]
+NodeDict = Union[Dict[T, TNode]]
+NodeId = Union[T, TNode]  # A node can be identified by its name or by itself
 
 
-class Node:
+class Node(Generic[T]):
     """
     Base class for representing a tree and a node a in a tree (same thing)
     """
-    def __init__(self,
-                 name: str,
-                 parent: Union[None, "Node"] = None,
-                 tree: Union[None, Dict[NodeId, "Node"]] = None) -> None:
-        if (tree is not None and name in tree) or (parent is not None and name in parent.tree):
-            raise ValueError(f"Node({name}) already exists. Node names must be unique")
-        self.name: NodeId = NodeId(name)
-        self.parent: Union[None, "Node"] = parent
-        self.children: List["Node"] = []
+    def __init__(self: TNode,
+                 id_: T,
+                 name: Optional[str] = None,
+                 parent: Optional[TNode] = None,
+                 tree: Optional[NodeDict] = None) -> None:
+        if (tree is not None and id_ in tree) or (parent is not None and id_ in parent.tree):
+            raise ValueError(f"Node({id_}) already exists. Node names must be unique")
+        if name is None:
+            name = str(id_)
+        self.id: T = id_
+        self.name: str = name
+        self.parent = parent
+        self.children: List[TNode] = []
         self.depth: int = 0 if parent is None else parent.depth+1
         if tree is None:
             tree = {} if parent is None else parent.tree
-        self.tree: Dict[NodeId, "Node"] = tree
-        self.tree[self.name] = self
+        self.tree: NodeDict = tree
+        self.tree[self.id] = self
 
         if parent:
             self.parent.children.append(self)
 
-    def add(self, name: str) -> "Node":
+    def add(self: TNode, id_: T, name: Optional[str] = None) -> TNode:
         """
-        Create a new node with name `name`, and add it to the children of `self`
+        Create a new node with id `id`, and add it to the children of `self`
         """
-        node = Node(name, parent=self, tree=self.tree)
+        node = self.__class__(id_, name=name, parent=self, tree=self.tree)
         return node
 
-    def remove(self, name_or_node: NodeOrName) -> None:
+    def remove(self: TNode, id_or_node: NodeId) -> None:
         """
-        Remove a node from the children of `self`. You can select the node to remove whether by its name or by itself
+        Remove a node from the children of `self`. You can select the node to remove whether by its id or by itself
         """
-        node = self[name_or_node]
+        node = self[id_or_node]
         if node in self.children:
             node.detach()
         del node  # probably useless
 
-    def add_many(self, names: Iterable[str]) -> None:
+    def add_many(self: TNode, ids: Iterable[Union[T, Tuple[T, str]]]) -> None:
         """
         Create and add a bunch of new nodes from a list, see method `Node.add` for details
         """
-        for name in names:
-            self.add(name)
+        for id_ in ids:
+            if isinstance(id_, tuple):
+                self.add(*id_)
+            else:
+                self.add(id_)
 
-    def remove_many(self, names: Iterable[str]) -> None:
+    def remove_many(self, ids: Iterable[T]) -> None:
         """
         Remove a bunch of nodes from `self.children`, see method `Node.remove` for details
         """
-        for name in names:
-            self.remove(name)
+        for id_ in ids:
+            self.remove(id_)
 
-    def siblings(self) -> List["Node"]:
+    def siblings(self: TNode) -> List[TNode]:
         """
         Return the siblings of the node (i.e all other nodes that have the same parent)
         """
@@ -84,15 +96,15 @@ class Node:
             return []
         return [node for node in self.parent.children if node != self]
 
-    def to_edges(self) -> List[Tuple[NodeId, NodeId]]:
+    def to_edges(self) -> List[Tuple[T, T]]:
         """
         Return a list of `(child, parent)` tuples, each one representing an edge in the tree
         """
-        edges = [(node.name, node.parent.name) for node in self if node.parent is not None]
+        edges = [(node.id, node.parent.id) for node in self if node.parent is not None]
         return edges
 
     @classmethod
-    def from_edges(cls, edges: List[Tuple[str, str]], add_root: Union[bool, str, NodeId] = False, **init_params) -> "Node":
+    def from_edges(cls: Type[TNode], edges: List[Tuple[T, T]], add_root: Optional[T] = None, **init_params) -> TNode:
         """
         Build a tree from a list of `(child, parent)` tuples, each one representing an edge in the tree
         """
@@ -102,7 +114,7 @@ class Node:
         if len(roots) < 1:
             raise ValueError("Tree contains a cycle")
         elif len(roots) > 1:
-            if not add_root:
+            if add_root is None:
                 raise ValueError("Tree contains several roots. You must set a value for parameter 'add_root'")
             for root in roots:
                 edges.append((root, add_root))
@@ -118,30 +130,31 @@ class Node:
         tree.rebuild_depths()
         return tree
 
-
-
     @classmethod
-    def read_edge_list(cls, file: TextIO) -> List[Tuple[str, str]]:
+    def read_edge_list(cls, file: TextIO, preprocess: Optional[Callable[[str], T]] = None) -> List[Tuple[T, T]]:
         edges = []
         for line in file:
             child, parent = line.split()
+            if preprocess is not None:
+                child, parent = preprocess(child), preprocess(parent)
             edges.append((child, parent))
         return edges
 
     @classmethod
-    def write_edge_list(cls, edges: Iterable[Tuple[str, str]], file: TextIO):
+    def write_edge_list(cls, edges: Iterable[Tuple[T, T]], file: TextIO):
         for edge in edges:
             print(*edge, file=file)
 
     @classmethod
-    def from_file(cls, filename: str, add_root: Union[bool, str, NodeId] = False) -> "Node":
-        with open(filename, "r", encoding="utf8") as f:
-            edges = cls.read_edge_list(f)
+    def from_file(cls: Type[TNode], filename: str, add_root: Optional[T] = None,
+                  preprocess: Optional[Callable[[str], T]] = None) -> TNode:
+        with open(filename, "r", encoding="utf8") as file:
+            edges = cls.read_edge_list(file, preprocess=preprocess)
         return cls.from_edges(edges, add_root=add_root)
 
     def to_file(self, filename: str):
-        with open(filename, "w", encoding="utf8") as f:
-            self.write_edge_list(self.to_edges(), f)
+        with open(filename, "w", encoding="utf8") as file:
+            self.write_edge_list(self.to_edges(), file)
 
     @property
     def is_root(self) -> bool:
@@ -156,10 +169,10 @@ class Node:
         return len(self.children)
 
     @property
-    def root(self) -> "Node":
+    def root(self: TNode) -> TNode:
         return self
 
-    def __iter__(self) -> Generator["Node", None, None]:
+    def __iter__(self: TNode) -> Generator[TNode, None, None]:
         """
         Return all nodes in the tree, including itself (i.e non-strict successors of `self`)
         """
@@ -167,7 +180,7 @@ class Node:
             if node <= self:
                 yield node
 
-    def iter_children(self) -> Generator["Node", None, None]:
+    def iter_children(self: TNode) -> Generator[TNode, None, None]:
         """
         Return all nodes in the tree, excluding itself (i.e strict successors of `self`)
         """
@@ -175,7 +188,7 @@ class Node:
             if node < self:
                 yield node
 
-    def rebuild_depths(self, from_depth: Union[bool, int] = None) -> None:
+    def rebuild_depths(self, from_depth: Union[None, bool, int] = None) -> None:
         """
         Recompute depths for all successors of `self`
         """
@@ -211,15 +224,7 @@ class Node:
         # Name are supposed to be unique in the tree
         return hash(self.name)
 
-    def __contains__(self, item) -> bool:
-        """
-        # TODO: what's the diff with `self < other`?
-        """
-        if isinstance(item, type(self)):
-            item = item.name
-        return item in self.tree and self.tree[item] <= self
-
-    def __getitem__(self, item) -> "Node":
+    def __getitem__(self: TNode, item) -> TNode:
         """
         Get a node in the tree from its name
         """
@@ -232,8 +237,8 @@ class Node:
             return node
         raise KeyError(f"Can't find item '{item}' in the subtree of {repr(self)}")
 
-    def bfs(self, start: Union[None, NodeOrName] = None, max_depth: Number = INF, max_nodes: Number = INF,
-            halting_func: Union[None, Callable] = None, **kwargs) -> Generator["Node", None, None]:
+    def bfs(self: TNode, start: Optional[NodeId] = None, max_depth: Number = INF, max_nodes: Number = INF,
+            halting_func: Optional[Callable[..., bool]] = None, **kwargs) -> Generator[TNode, None, None]:
         """Breadth-first search over the tree"""
         if start is None:
             start = self.root
@@ -251,8 +256,8 @@ class Node:
             for children in node.children:
                 unvisited.append(children)
 
-    def dfs(self, start: Union[None, NodeOrName] = None, max_depth: Number = INF, max_nodes: Number = INF,
-            halting_func: Union[None, Callable] = None, **kwargs) -> Generator["Node", None, None]:
+    def dfs(self: TNode, start: Optional[NodeId] = None, max_depth: Number = INF, max_nodes: Number = INF,
+            halting_func: Union[None, Callable[..., bool]] = None, **kwargs) -> Generator[TNode, None, None]:
         """Depth-first search over the tree"""
         if start is None:
             start = self.root
@@ -269,7 +274,7 @@ class Node:
             for children in node.children:
                 unvisited.append(children)
 
-    def detach(self, base_node: Union[None, NodeOrName] = None, update_tree: bool = True) -> None:
+    def detach(self, base_node: Optional[NodeId] = None, update_tree: bool = True) -> None:
         """
         Remove the edge between `self` and its parent. Alternatively, you can detach `base_start` instead of `self`
         """
@@ -288,7 +293,7 @@ class Node:
             base_node.tree = new_tree
         base_node.rebuild_depths()
 
-    def attach(self, parent: "Node", update_tree: bool = True) -> None:
+    def attach(self: TNode, parent: TNode, update_tree: bool = True) -> None:
         """
         Add an edge between `self` and `parent`.
         """
@@ -296,20 +301,20 @@ class Node:
             self.detach()
         if update_tree:
             for node in self:
-                parent.tree[node.name] = node
+                parent.tree[node.id] = node
                 node.tree = parent.tree
         parent.children.append(self)
         self.parent = parent
         self.rebuild_depths(from_depth=parent.depth+1)
 
-    def move(self, to: "Node") -> None:
+    def move(self: TNode, to: TNode) -> None:
         """
         Move an node to the node `to` (i.e remove the current edge to its parent and add an edge to `to`)
         """
         self.detach(update_tree=False)
         self.attach(to, update_tree=False)
 
-    def iter_branch(self) -> Generator["Node", None, None]:
+    def iter_branch(self: TNode) -> Generator[TNode, None, None]:
         """
         Iterate along the unique path between `self` and the root
         """
@@ -319,7 +324,7 @@ class Node:
         else:
             yield from self.parent.iter_branch()
 
-    def print(self, start=None, max_depth=5, as_string=False, **kwargs):
+    def print(self: TNode, start: Optional[NodeId] = None, max_depth=5, as_string=False, **kwargs):
         """
         Provide a (text-based) visual representation of the tree.
 
@@ -340,13 +345,13 @@ class Node:
                     return writer.getvalue()
         print_tree(start, max_depth=max_depth, **kwargs)
 
-    def custom_print(self, func, start=None, max_depth=5, **kwargs):
+    def custom_print(self: TNode, func, start: Optional[NodeId] = None, max_depth=5, **kwargs):
         """
         Define a custom printing function for the node. See method `Node.print` for the other parameters.
         """
         self.print(start, max_depth, nameattr=func, **kwargs)
 
-    def plot(self, max_depth: Union[None, int] = 4, max_width : Union[None, int] = 7, custom_label=None, **params):
+    def plot(self, max_depth: Optional[int] = 4, max_width: Optional[int] = 7, custom_label=None, **params):
         """
         Plot a tree using matplotlib and `libs.viz.nary_tree` module
         """
@@ -355,7 +360,7 @@ class Node:
         coords, edges = treeplot.get_coords(self, max_depth=max_depth, max_width=max_width)
         treeplot.plot_tree(coords, edges, labels=custom_label, **params)
 
-    def _build_at_depth(self) -> Dict[int, List["Node"]]:
+    def _build_at_depth(self: TNode) -> Dict[int, List[TNode]]:
         at_depth = dict()
         for node in self:
             if node.depth not in at_depth:
