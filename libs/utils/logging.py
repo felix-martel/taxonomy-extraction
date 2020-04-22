21f1c277cebc7c5d4f8bbdaa7d677169e4bdb9c8
felix <felix.martel@polytechnique.edu>
2020-04-22 14:30:43 -0400

    Add class Tee for logging module

--- libs/utils/logging.py
+++ libs/utils/logging.py
@@ -20,3 +20,37 @@ except (ImportError, AssertionError):
     def beep(disable=False):
         if not disable:
             MessageBeep()
+
+import traceback
+import sys
+
+
+# Context manager that copies stdout and any exceptions to a log file
+class Tee(object):
+    def __init__(self, filename):
+        self.file = open(filename, 'w')
+        self.stdout = sys.stdout
+        self._history = ""
+
+    def __enter__(self):
+        sys.stdout = self
+
+    def __exit__(self, exc_type, exc_value, tb):
+        sys.stdout = self.stdout
+        if exc_type is not None:
+            exc = traceback.format_exc()
+            self.file.write(exc)
+            self._history += exc
+        self.file.close()
+
+    def write(self, data):
+        self.file.write(data)
+        self.stdout.write(data)
+        self._history += data
+
+    def flush(self):
+        self.file.flush()
+        self.stdout.flush()
+
+    def history(self):
+        return self._history
