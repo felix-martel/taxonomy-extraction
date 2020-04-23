0393560349fdbcd4fd96ff1685cc6720d7bc4961
felix <felix.martel@polytechnique.edu>
2020-04-23 19:13:45 -0400

    Add toy dataset to registered taxonomies

--- libs/taxonomy/_files.py
+++ libs/taxonomy/_files.py
@@ -1,10 +1,12 @@
 from typing import Dict
 
-
+# TODO: use config
 DBPEDIA_FULL = "data/taxonomy/full.txt"
 DBPEDIA_SMALL = "data/taxonomy/small.txt"
+DBPEDIA_TOY = "data/taxonomy/toy.txt"
 
 registered: Dict[str, str] = {
     "full": DBPEDIA_FULL,
-    "small": DBPEDIA_SMALL
+    "small": DBPEDIA_SMALL,
+    "toy": DBPEDIA_TOY
 }
