c6ffd305780c4e845011f1648ed81e8060442458
felix <felix.martel@polytechnique.edu>
2020-09-03 10:27:29 +0200

    Add GraphSampler and NaiveGraphSampler to `sampling.__init__.py`

--- libs/sampling/__init__.py
+++ libs/sampling/__init__.py
@@ -4,3 +4,6 @@ A module for the recursive sampling step of our approach.
 It uses an `Axiom` and a `KnowledgeGraph`, and sample entities from the
 graph that verify the axiom.
 """
+
+
+from .sampler import GraphSampler, NaiveGraphSampler
