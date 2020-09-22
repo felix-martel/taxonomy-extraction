0a648a8cae9751c9431e194323d761482aab2cf3
felix <felix.martel@polytechnique.edu>
2020-09-22 22:35:59 +0200

    Cleaning for hard_mapping implementation

--- libs/extraction/global_max.py
+++ libs/extraction/global_max.py
@@ -1,11 +1,7 @@
-import os
-
 from scipy.optimize import linear_sum_assignment
-from libs.utils import Timer
-from libs.metrics import f_score
-import numpy as np
 
-import libs.extraction.common as common
+from ..utils import Timer
+from .common import mapping_to_axioms
 
 
 def compute_mapping(F, root=None, allow_root=False, verbose=False, **kwargs):
@@ -15,15 +11,16 @@ def compute_mapping(F, root=None, allow_root=False, verbose=False, **kwargs):
         F.iloc[root.id] = 0.0
     
     # Compute F-matrix : Fij = F1(clu_i, cls_j)
-        #F = get_F_matrix(clustering)
-        # Compute a mapping m: cls -> clu that maximizes sum_j F1(m(cls_j), cls_j)
-        with Timer():
-            rows, cols = linear_sum_assignment(-F.values)
-        int_to_cls = {i: cls for i, cls in enumerate(F.columns)}
-        cls_to_clu = {int_to_cls[i]: clust for clust, i in zip(rows, cols)}
-        return cls_to_clu
+    # F = get_F_matrix(clustering)
+    # Compute a mapping m: cls -> clu that maximizes sum_j F1(m(cls_j), cls_j)
+    with Timer():
+        rows, cols = linear_sum_assignment(-F.values)
+    int_to_cls = {i: cls for i, cls in enumerate(F.columns)}
+    cls_to_clu = {int_to_cls[i]: clust for clust, i in zip(rows, cols)}
+    return cls_to_clu
+
 
 def extract_axioms(F, root=None, allow_root=False, verbose=False, **kwargs):    
     clu = kwargs.pop("clu")
     cls_to_clu = compute_mapping(F, root, allow_root, verbose, **kwargs)
-    return common.mapping_to_axioms(cls_to_clu, clu)
+    return mapping_to_axioms(cls_to_clu, clu)
