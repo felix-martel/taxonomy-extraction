70b3f30bbe2832c42ea1fd8596dfb4a8118ecc4c
felix <felix.martel@polytechnique.edu>
2020-09-22 22:38:29 +0200

    Add aliases for hard_ and soft_mapping in 'libs.extraction'

--- libs/extraction/__init__.py
+++ libs/extraction/__init__.py
@@ -2,3 +2,8 @@ from .global_max import extract_axioms as extract_global
 from .local_max import extract_axioms as extract_local
 from .probabilistic import extract_axioms as extract_proba
 from .extractor import extract_axioms, TaxonomyExtractor
+
+
+# Aliases
+hard_mapping = extract_global
+soft_mapping = extract_proba
